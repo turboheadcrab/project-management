9e81086acb796bbd38c74be30ab068210855daeb
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2022-02-14 00:39:16 -0500

    Improving the Homepage

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,6 +1,17 @@
 // For a pie chart
-var myPieChart = new Chart(ctx, {
+new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
-    data: data,
-    options: options
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+            borderColor: 'rgb(255, 99, 132)',
+            data: [5, 10, 5]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
 })

51d7df89050691bc1aaed47e7cd0e21c435aa250
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2022-02-14 22:25:35 -0500

    Using Model Attributes with JavaScript in Spring and Thymeleaf

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,17 +1,41 @@
+var niceChartData = decodeHtml(chartData);
+var chartJsonArray = JSON.parse(niceChartData);
+
+var arrayLength = chartJsonArray.length;
+
+var numericData = [];
+var labelData = [];
+
+for (var i = 0; i < arrayLength; i++) {
+    numericData[i] = chartJsonArray[i].value;
+    labelData[i] = chartJsonArray[i].label;
+}
+
 // For a pie chart
 new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March'],
+        labels: labelData,
         datasets: [{
             label: 'My First dataset',
             backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
-            borderColor: 'rgb(255, 99, 132)',
-            data: [5, 10, 5]
+            data: numericData
         }]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        title: {
+            display: true,
+            text: 'Project Statuses'
+        }
+    }
 })
+
+// "[{"value": 1, "label": "COMPLETED"}, {"value": 2, "label": "INPROGRESS"}, {"value": 1, "label": "NOTSTARTED"}]"
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
